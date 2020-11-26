3024eebc0d069671d2aac98f026b611c6d3c4b69
Sepo <HGrell@outlook.com>
2020-11-26 14:33:18 +0700

    start

---
+++ labagit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace labagit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
